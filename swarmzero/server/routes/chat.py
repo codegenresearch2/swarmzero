10062680ae658a83f91b1d494ef1e32b3d4d6a88
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:09:38 +0000

    Reset to ec077461138cfd3f8a35cc913d1977b775901def and made changes for LLM Round 1

--- swarmzero/server/routes/chat.py
+++ swarmzero/server/routes/chat.py
@@ -92,7 +92,7 @@ def setup_chat_routes(router: APIRouter, id, sdk_context: SDKContext):
         if files:
             stored_files = await insert_files_to_index(files, id, sdk_context)
 
-        last_message, _ = await validate_chat_data(chat_data_parsed)
+        last_message, chat_history = await validate_chat_data(chat_data_parsed)
 
         response = await inject_additional_attributes(
             lambda: chat_manager.generate_response(db_manager, last_message, stored_files), {"user_id": user_id}
@@ -105,7 +105,6 @@ def setup_chat_routes(router: APIRouter, id, sdk_context: SDKContext):
         session_id: str = Query(...),
         db: AsyncSession = Depends(get_db),
     ):
-
         llm_instance, enable_multi_modal = get_llm_instance(id, sdk_context)
         chat_manager = ChatManager(llm_instance, user_id=user_id, session_id=session_id)
         db_manager = DatabaseManager(db)
@@ -129,7 +128,6 @@ def setup_chat_routes(router: APIRouter, id, sdk_context: SDKContext):
 
     @router.get("/all_chats")
     async def get_all_chats(user_id: str = Query(...), db: AsyncSession = Depends(get_db)):
-
         llm_instance, enable_multi_modal = get_llm_instance(id, sdk_context)
         chat_manager = ChatManager(llm_instance, user_id=user_id, session_id="")
         db_manager = DatabaseManager(db)
